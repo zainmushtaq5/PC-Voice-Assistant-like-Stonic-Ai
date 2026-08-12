5d680faa31c43e7642c7f4dade61599ce966732f
Your Name <your_email@example.com>
2026-08-12 17:57:58 +0500

    Remove venv from tracking

--- venv311/Lib/site-packages/huggingface_hub/inference/_providers/baseten.py
+++
@@ -1,6 +0,0 @@
-from ._common import BaseConversationalTask
-
-
-class BasetenConversationalTask(BaseConversationalTask):
-    def __init__(self):
-        super().__init__(provider="baseten", base_url="https://inference.baseten.co")
